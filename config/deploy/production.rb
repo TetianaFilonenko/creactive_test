548ab224f46faa7d980b04e205bd18c3d663ea85
Tania Filonenko <Tania.Filonenko@gmail.com>
2015-11-02 13:45:07 +0200

    deploy settings

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-set :deploy_to, '/home/creactive/creactive_app'
+set :deploy_to, '/home/deployer/creactive/creactive_app'
 
 # RVM-specific config
 set :application, 'creactive'
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,7 +1,7 @@
-set :deploy_to, '/home/creactive/creactive_app'
+set :deploy_to, '/home/deployer/creactive/creactive_app'
 
 # RVM-specific config
-set :application, 'fresh-fw-stage'
+set :application, 'creactive'
 set :branch, "staging"
 set :rails_env, :staging
 
